838fd9b81b41061c2772d8df1587c87f5a8df0d7
K.POOJASHREE <110245690+Poojashree4@users.noreply.github.com>
2025-03-17 11:49:09 +0530

    Update FoodDisplay.jsx
    added pagination

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,27 +1,62 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
-const FoodDisplay = ({category}) => {
-    const {food_list} =useContext(StoreContext)
+import React, { useContext, useState } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+  
+  const itemsPerPage = 8; 
+  const [currentPage, setCurrentPage] = useState(1);
+
+ 
+  const filteredFoodList = food_list.filter((item) => category === "All" || category === item.category);
+
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredFoodList.slice(indexOfFirstItem, indexOfLastItem);
+
+ 
+  const totalPages = Math.ceil(filteredFoodList.length / itemsPerPage);
+
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className="food-display-list">
-            {food_list.map((item,index)=>{
-                if(category==="All" || category===item.category){
-                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-                }
-                
-            })
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {currentItems.map((item, index) => (
+          <FoodItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
+      </div>
 
-            }
-        </div>
-
-
+ 
+      <div className="pagination">
+        <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === totalPages}> Next</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
